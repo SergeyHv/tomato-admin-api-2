97a769354ce6eecd00a89d6fb96611e63766d7af
SergeyHv <skhvostenok@yandex.ru>
2025-12-12 11:10:13 +0300

    Update list.js

--- api/sheets/list.js
+++ api/sheets/list.js
@@ -1,4 +1,4 @@
-import { getSheetsClient } from '../_lib/googleClient';
+import { getSheetsClient } from '../lib/googleClient';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
