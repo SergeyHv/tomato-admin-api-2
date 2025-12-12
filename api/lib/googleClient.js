b5dfe020a975c1d92075a04379a19876ccb2a524
SergeyHv <skhvostenok@yandex.ru>
2025-12-12 11:08:38 +0300

    Update googleClient.js

--- api/lib/googleClient.js
+++ api/lib/googleClient.js
@@ -1,1 +1,18 @@
-сс
+import { google } from "googleapis";
+
+export async function getSheetsClient() {
+  const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
+
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: [
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/drive"
+    ]
+  });
+
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+
+  return sheets;
+}
